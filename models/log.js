a493c9b86ea2d788d65e7a1aae644943ac5d2716
Jisa_Mathew <35719226+jisamathew@users.noreply.github.com>
2023-08-16 10:02:38 +0530

    Update log.js

--- models/log.js
+++ models/log.js
@@ -1,14 +1,11 @@
 const mongoose = require('mongoose');
 // Define collection and schema
-const Schema = mongoose.Schema
-const ObjectId = Schema.Types.ObjectId
 
-let LogSchema = new Schema({
-   _id:{type:ObjectId},
+let LogSchema = new mongoose.Schema({
    lei: { type: String },
    pastRecord: { type: String },
    currentRecord: { type: String },
    dateVerified: { type: String },
-   wallet: {  type: String },
+   wallet: {  type: String }
 })
 module.exports = mongoose.model('Log', LogSchema)
